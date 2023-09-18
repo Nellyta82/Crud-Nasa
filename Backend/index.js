c23af2a4a3da35edd018322db8a96e4db2b52287
repoNelly <nellytachirinos@gmail.com>
2023-09-18 14:24:22 -0300

    otra prueba

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-const Server = require("./models/server");
+const Server = require("../Backend/models/server");
 
 const server = new Server();
 
-server.listen();
+server.listen();s
