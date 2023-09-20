76725ed5f2b72188fcd6ea446e0dd1595b8fcaf4
repoNelly <nellytachirinos@gmail.com>
2023-09-20 09:50:08 -0300

    Cambiando

--- Frontend/src/components/AgregarImagen.jsx
+++ Frontend/src/components/AgregarImagen.jsx
@@ -19,7 +19,7 @@ function AgregarImagen(){
             titulo:titulo,
             explicacion:explicacion
         }
-        axios.post('https://crud-nasa-api.vercel.app/nasa/', newImage)
+        axios.post('http://localhost:8080/nasa/', newImage)
         .then(res => console.log(res.data))
         .then(err => console.log(err))
         alert('Imagen agregada a la Base de Datos')
